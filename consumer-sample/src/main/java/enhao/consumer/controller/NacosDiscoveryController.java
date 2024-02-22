cf62866a5dd8edca8c35856c1923cbc865cd089b
liangenhao <liangenhao@hotmail.com>
2024-02-22 14:54:09 +0800

    feat: 通过 Nacos Config 实现 Sentinel 集群流控的动态数据源配置持久化

--- consumer-sample/src/main/java/enhao/consumer/controller/NacosDiscoveryController.java
+++ consumer-sample/src/main/java/enhao/consumer/controller/NacosDiscoveryController.java
@@ -21,7 +21,7 @@ public class NacosDiscoveryController {
 
     @GetMapping("/echo/app-name")
     public String echoAppName(){
-        //使用 LoadBalanceClient 和 RestTemolate 结合的方式来访问
+        //使用 LoadBalanceClient 和 RestTemplate 结合的方式来访问
         ServiceInstance serviceInstance = loadBalancerClient.choose("provider-sample");
         String url = String.format("http://%s:%s/echo/%s",serviceInstance.getHost(),serviceInstance.getPort(),appName);
         System.out.println("request url:"+url);
