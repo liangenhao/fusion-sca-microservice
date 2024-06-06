81fb169f58c7deae1da1c07701217f4bdecbfec7
liangenhao <liangenhao@hotmail.com>
2024-06-06 19:24:32 +0800

    feat: 新增网关

---
+++ fusion-gateway/src/main/java/io/fusion/gateway/config/GatewayConfig.java
@@ -0,0 +1,10 @@
+package io.fusion.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author enhao
+ */
+@Configuration(proxyBeanMethods = false)
+public class GatewayConfig {
+}
