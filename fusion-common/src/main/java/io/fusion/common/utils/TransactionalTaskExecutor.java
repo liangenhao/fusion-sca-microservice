8fba51040ca1e2efe5f6ba5e48aa0285083d1475
liangenhao <liangenhao@hotmail.com>
2025-03-19 21:13:28 +0800

    feat: 事务执行器

--- fusion-common/src/main/java/io/fusion/common/utils/TransactionalTaskExecutor.java
+++ fusion-common/src/main/java/io/fusion/common/utils/TransactionalTaskExecutor.java
@@ -1,7 +1,11 @@
 package io.fusion.common.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import java.util.function.Consumer;
 
 /**
  * 事务执行器
@@ -9,6 +13,8 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  * @author enhao
  */
 public class TransactionalTaskExecutor {
+
+    private static final Logger log = LoggerFactory.getLogger(TransactionalTaskExecutor.class);
 
     /**
      * 在事务提交后执行指定的操作，如果没有事务，则直接执行。
@@ -38,4 +44,91 @@ public class TransactionalTaskExecutor {
             task.run();
         }
     }
+
+    public static void runCommit(Consumer<Boolean> beforeCommit, Runnable beforeCompletion,
+                                 Runnable afterCommit, Consumer<Integer> afterCompletion) {
+        if (TransactionSynchronizationManager.isSynchronizationActive()) {
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                @Override
+                public void beforeCommit(boolean readOnly) {
+                    if (beforeCommit != null) {
+                        beforeCommit.accept(readOnly);
+                    }
+                }
+
+                @Override
+                public void beforeCompletion() {
+                    if (beforeCompletion != null) {
+                        beforeCompletion.run();
+                    }
+                }
+
+                @Override
+                public void afterCommit() {
+                    if (afterCommit != null) {
+                        afterCommit.run();
+                    }
+                }
+
+                @Override
+                public void afterCompletion(int status) {
+                    if (afterCompletion != null) {
+                        afterCompletion.accept(status);
+                    }
+                }
+            });
+            return;
+        }
+
+        // 若不处于事务中，模拟事务同步调用流程
+        try {
+            triggerBeforeCommit(beforeCommit, false);
+            triggerBeforeCompletion(beforeCompletion);
+        } catch (Throwable t) {
+            triggerBeforeCompletion(beforeCompletion);
+
+            triggerAfterCompletion(afterCompletion, TransactionSynchronization.STATUS_UNKNOWN);
+            throw t;
+        }
+
+        try {
+            triggerAfterCommit(afterCommit);
+        } finally {
+            triggerAfterCompletion(afterCompletion, TransactionSynchronization.STATUS_COMMITTED);
+        }
+
+    }
+
+    private static void triggerBeforeCommit(Consumer<Boolean> beforeCommit, boolean isReadOnly) {
+        if (beforeCommit != null) {
+            beforeCommit.accept(isReadOnly);
+        }
+    }
+
+    private static void triggerBeforeCompletion(Runnable beforeCompletion) {
+        if (beforeCompletion != null) {
+            try {
+                beforeCompletion.run();
+            } catch (Throwable t) {
+                log.info("beforeCompletion threw exception", t);
+            }
+        }
+    }
+
+    private static void triggerAfterCommit(Runnable afterCommit) {
+        if (afterCommit != null) {
+            afterCommit.run();
+        }
+    }
+
+    private static void triggerAfterCompletion(Consumer<Integer> afterCompletion, int status) {
+        if (afterCompletion != null) {
+            try {
+                afterCompletion.accept(status);
+            } catch (Throwable t) {
+                log.info("afterCompletion threw exception", t);
+            }
+        }
+    }
+
 }
